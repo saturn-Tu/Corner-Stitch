dc457bd250ee7b38879a4b349680004a686f538b
Kevin <kk2452080486@gmail.com>
2020-02-04 17:49:28 +0800

    Update showTile.py

--- showTile.py
+++ showTile.py
@@ -5,27 +5,22 @@ file = open('C:\\Users\\kevin\\Documents\\GitHub\\Corner-Stitch\\output.txt','r'
 fig = plt.subplot(1,1,1)
 fig.set_title("original_same_color")
 line = file.readline()
-color_list=['b','g','r','y','k']
+color_list=['b','r']
 color_index=0
 num_line=0
 print("line, ", line)
 while line:
-    num_line=num_line+1
-    print("process line:",num_line)
     line_split=line.split(" ")
-    line_split.pop(-1)
+    if line_split[0] == 'TILE':
+        color_index = 1;
+    else:
+        color_index = 0;
     line_split.pop(0)
     line_split_f=[float(x) for x in line_split]
     for i in range(0,len(line_split)-3,2):
-        point = [(   line_split_f[i], line_split_f[i+1]), (line_split_f[i+2], line_split_f[i+3])]
+        point = [(line_split_f[i], line_split_f[i+1]), (line_split_f[i+2], line_split_f[i+3])]
         (xpoint, ypoint) = zip(*point)
-        ##color_index=int(num_line/300);
-        print("print")
         fig.plot(xpoint, ypoint,color_list[color_index])
-    color_index=color_index+1
-    if(color_index==5):
-        color_index=0
-    print('sleep')
     line = file.readline()
 plt.show() 
 file.close()
